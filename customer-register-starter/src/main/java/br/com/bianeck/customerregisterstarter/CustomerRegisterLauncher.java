1ef4f580e95a2e2e628eca811a538554aa004346
Thiago Moreira Bianeck <thiagobianeck@gmail.com>
2020-12-15 21:51:54 -0300

    Estrutura Inicial do Projeto

--- customer-register-starter/src/main/java/br/com/bianeck/customerregisterstarter/CustomerRegisterLauncher.java
+++ customer-register-starter/src/main/java/br/com/bianeck/customerregisterstarter/CustomerRegisterLauncher.java
@@ -2,12 +2,14 @@ package br.com.bianeck.customerregisterstarter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-public class CustomerRegisterStarterApplication {
+@EnableTransactionManagement
+public class CustomerRegisterLauncher {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CustomerRegisterStarterApplication.class, args);
+		SpringApplication.run(CustomerRegisterLauncher.class, args);
 	}
 
 }
--- customer-register-starter/src/test/java/br/com/bianeck/customerregisterstarter/CustomerRegisterLauncherTests.java
+++ customer-register-starter/src/test/java/br/com/bianeck/customerregisterstarter/CustomerRegisterLauncherTests.java
@@ -1,10 +1,10 @@
-package br.com.bianeck.customerregisterjpa;
+package br.com.bianeck.customerregisterstarter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CustomerRegisterJpaApplicationTests {
+class CustomerRegisterLauncherTests {
 
 	@Test
 	void contextLoads() {
